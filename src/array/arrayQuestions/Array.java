a857404946b1003cecbd18dca58a56b2876b8308
Yasirkhan787 <yasirkh398@gmail.com>
2025-07-31 15:38:11 +0500

    Problem 4 & Problem 5 in arrayQuestions

--- src/array/arrayQuestions/Array.java
+++ src/array/arrayQuestions/Array.java
@@ -79,4 +79,70 @@ public class Array {
 
         return maxCount;
     }
+
+    /*
+        Problem 4: Move all zeros to end of array
+        Given an array of integers arr[], the task is to move all the zeros to the end of the array while maintaining the relative order of all non-zero elements.
+
+        Examples:
+
+        Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+        Output: arr[] = [1, 2, 4, 3, 5, 0, 0, 0]
+        Explanation: There are three 0s that are moved to the end.
+
+        Input: arr[] = [10, 20, 30]
+        Output: arr[] = [10, 20, 30]
+        Explanation: No change in array as there are no 0s.
+
+        Input: arr[] = [0, 0]
+        Output: arr[] = [0, 0]
+        Explanation: No change in array as there are all 0s.
+     */
+    public void pushZerosToEnd(int[] arr) {
+
+        int count = 0;
+
+        for (int i = 0; i<arr.length; i++){
+            if (arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr [count];
+                arr[count] = temp;
+                count++;
+            }
+        }
+    }
+    /*
+        Problem 5: Reverse an Array in groups of given size
+        Given an array arr[] and an integer k, find the array after reversing every subarray of consecutive k elements in place. If the last subarray has fewer than k elements, reverse it as it is. Modify the array in place, do not return anything.
+
+        Examples:
+
+        Input: arr[] = [1, 2, 3, 4, 5, 6, 7, 8], k = 3
+        Output: [3, 2, 1, 6, 5, 4, 8, 7]
+        Explanation: Elements is reversed: [1, 2, 3] → [3, 2, 1], [4, 5, 6] → [6, 5, 4], and the last group [7, 8](size < 3) is reversed as [8, 7].
+
+        Input: arr[] = [1, 2, 3, 4, 5], k = 3
+        Output: [3, 2, 1, 5, 4]
+        Explanation: First group consists of elements 1, 2, 3. Second group consists of 4, 5.
+
+        Input: arr[] = [5, 6, 8, 9], k = 5
+        Output: [9, 8, 6, 5]
+        Explanation: Since k is greater than array size, the entire array is reversed.
+     */
+    public void reverseInGroups(int[] arr, int k){
+
+        for (int i = 0; i< arr.length; i += k){
+
+            int left = i;
+            int right = Math.min((i + k) - 1, (arr.length) - 1);
+
+            while (left<right){
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right]  = temp;
+                left++;
+                right--;
+            }
+        }
+    }
 }
